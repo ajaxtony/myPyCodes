98d8e764e8db3f1ecf095ee06523cd215555457b
ajaxtony <57217812@qq.com>
2023-05-23 15:14:40 +0800

    使用 Colaboratory 创建

---
+++ test-colab.py
@@ -0,0 +1,37 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyNgj8ajOKhdRn5Im0vejr4x",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/ajaxtony/myPyCodes/blob/main/test-colab.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "markdown",
+      "source": [],
+      "metadata": {
+        "id": "YAyOALEwdJue"
+      }
+    }
+  ]
+}
